f8bf3362a1e04d0837d658e512e3146f63c3d8f9
Billy Mills <wmills@unca.edu>
2012-09-27 10:11:01 -0400

    tweaking timing test

--- src/timing_tests/aqueue_timing.cpp
+++ src/timing_tests/aqueue_timing.cpp
@@ -16,23 +16,26 @@ int main() {
 	start = clock(); //get starting ticks
 	
 	//start code to test here
+	cout << "Running timing test for array-based queue implementation" << endl;
 	AQueue* queue = new AQueue(10);
-	for(int i=1;i<=1000000;++i){
-		queue->enqueue(i);
+	for(long i=1;i<=100000;++i){
+		for(long j=1;j<=100;++j){
+			queue->enqueue(j);
+		}
 	}	
-	cout << queue->size() << endl;
-	for(int j=1;j<=1000000;++j) {
-		queue->dequeue();
+	cout << "The queue is currently " << queue->size() << " elements in size" << endl;
+	for(long x=1;x<=100000;++x) {
+		for(long y=1;y<=100;++y){
+			queue->dequeue();
+		}
 	}
-	cout << queue->size() << endl;
-	cout << queue->isEmpty() << endl;
-	cout << "hello" << endl;
+	cout << "The queue is now " << queue->size() << " elements in size" << endl;
 	delete queue;
 	//end code being tested here
 
 	end = clock(); //get ending ticks
 	elapsed = end-start; //calculate total elapsed ticks
 	seconds = (double) elapsed/CLOCKS_PER_SEC; 	//convert to seconds
-	std::cout << "raw array elapsed time: " << seconds << " seconds " << std::endl;
+	cout << "Elapsed time for this procedure is : " << seconds << " seconds " << endl;
 	return 0;
 }
