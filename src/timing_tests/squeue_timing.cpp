c88e21989493eb7f1a53350538b18141a8ed941f
Billy Mills <wmills@unca.edu>
2012-09-27 10:28:04 -0400

    tweaking timing test

--- src/timing_tests/squeue_timing.cpp
+++ src/timing_tests/squeue_timing.cpp
@@ -16,22 +16,26 @@ int main() {
 	start = clock(); //get starting ticks
 	
 	//start of code to be timed
+	cout << "Running timing test for std list-based queue implementation" << endl;
 	SQueue* queue = new SQueue();
-	for(int i=1;i<=1000000;++i){
-		queue->enqueue(i);
+	for(long i=1;i<=100000;++i){
+		for(long j=1;j<=100;++j){
+			queue->enqueue(j);
+		}
 	}
-	cout << queue->size() << endl;
-	for (int j=1;j<=1000000;++j){
-		queue->dequeue();
+	cout << "The queue is currently " << queue->size() << " elements is size" << endl;
+	for(long x=1;x<=100000;++x){
+		for(long y=1;y<=100;++y){
+			queue->dequeue();
+		}
 	}
-	cout << queue->size() << endl;
-	cout << queue->isEmpty() << endl;
+	cout << "The queue is now " << queue->size() << " elements in size" << endl;
 	delete queue;
 	//end of code to be timed
 
 	end = clock(); //get ending ticks
 	elapsed = end-start; //calculate total elapsed ticks
 	seconds = (double) elapsed/CLOCKS_PER_SEC; //convert to seconds
-	cout << "elapsed time: " << seconds << " seconds " << endl;
+	cout << "Elapsed time for this procedure is : " << seconds << " seconds " << endl;
 	return 0;
 }
